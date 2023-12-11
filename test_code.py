771e7ed60db39b4428a860291915bb9a032fe828
Olsens11 <Olsens11@gmail.com>
2023-12-11 17:44:42 -0500

    Initial commit

--- test_code.py
+++ test_code.py
@@ -1,7 +1,7 @@
 # test_code.py
 
 def main():
-    print("Hello, Raspberry Pi! We Updated it!! again! for the first time!")
+    print("Hello, Raspberry Pi! This one is for all the little pis out there who never had a chance")
 
 if __name__ == "__main__":
     main()
