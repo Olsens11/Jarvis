b5b1f6cfcd5cf2b8f44e9ae4ea09e733e263e205
Olsens11 <Olsens11@gmail.com>
2023-12-11 18:57:46 -0500

    Update all files and folders

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+original_dir=$(pwd)
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Pull changes from the "main" branch
+git pull origin main
+
+# Change back to the original directory
+cd "$original_dir"
+
+# Pause to see the output (remove this line if you don't want to pause)
+read -p "Press Enter to continue..."
