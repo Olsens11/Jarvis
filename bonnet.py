da80a3a9a937cb37a9e8a84bc4c5d80666608412
Olsens11 <Olsens11@gmail.com>
2023-12-11 20:04:59 -0500

    Update all files and folders

--- bonnet.py
+++ bonnet.py
@@ -6,11 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 # Set up the OLED Bonnet
 reset_pin = digitalio.DigitalInOut(board.D4)
 i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin)
-
-# Clear the display
-oled.fill(0)
-oled.show()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin, transform='flipy')
 
 # Create an image with a black background
 image = Image.new("1", (oled.width, oled.height), 0)
@@ -27,8 +23,8 @@ button_b = digitalio.DigitalInOut(board.D6)
 button_b.switch_to_input(pull=digitalio.Pull.UP)
 
 while True:
-    # Clear the display
-    oled.fill(0)
+    # Clear the image
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
     # Read button states
     button_a_state = not button_a.value
