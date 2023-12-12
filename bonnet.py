b03e15742663744b56a331327397563843e5e342
Olsens11 <Olsens11@gmail.com>
2023-12-11 20:36:34 -0500

    Update all files and folders

--- bonnet.py
+++ bonnet.py
@@ -1,12 +1,11 @@
 import board
-import busio
 from digitalio import DigitalInOut, Direction, Pull
 from PIL import Image, ImageDraw
 import adafruit_ssd1306
 
 # Set up the OLED Bonnet
 reset_pin = DigitalInOut(board.D4)
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = board.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin)
 
 # Create blank image for drawing.
@@ -18,14 +17,22 @@ image = Image.new("1", (width, height))
 # Get drawing object to draw on the image.
 draw = ImageDraw.Draw(image)
 
-# Initialize buttons
-button_A = DigitalInOut(board.D5)
-button_A.direction = Direction.INPUT
-button_A.pull = Pull.UP
+# Initialize buttons for joystick
+button_U = DigitalInOut(board.D17)
+button_U.direction = Direction.INPUT
+button_U.pull = Pull.UP
 
-button_B = DigitalInOut(board.D6)
-button_B.direction = Direction.INPUT
-button_B.pull = Pull.UP
+button_L = DigitalInOut(board.D27)
+button_L.direction = Direction.INPUT
+button_L.pull = Pull.UP
+
+button_R = DigitalInOut(board.D23)
+button_R.direction = Direction.INPUT
+button_R.pull = Pull.UP
+
+button_D = DigitalInOut(board.D22)
+button_D.direction = Direction.INPUT
+button_D.pull = Pull.UP
 
 button_C = DigitalInOut(board.D4)
 button_C.direction = Direction.INPUT
@@ -39,24 +46,28 @@ while True:
     # Clear the image
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # Read button states
-    button_A_state = not button_A.value
-    button_B_state = not button_B.value
+    # Read joystick button states
+    button_U_state = not button_U.value
+    button_L_state = not button_L.value
+    button_R_state = not button_R.value
+    button_D_state = not button_D.value
     button_C_state = not button_C.value
 
-    # Read joystick values
-    joystick_x_value = board.JOYSTICK_X
-    joystick_y_value = board.JOYSTICK_Y
-
-    # Determine text based on button states
-    if button_A_state and button_B_state:
-        text = "Both buttons pressed"
-    elif button_A_state:
-        text = "Button A pressed"
-    elif button_B_state:
-        text = "Button B pressed"
+    # Determine text based on joystick button states
+    if button_U_state and button_D_state and not button_L_state and not button_R_state and not button_C_state:
+        text = "Center"
+    elif button_U_state and not button_D_state and not button_L_state and not button_R_state and not button_C_state:
+        text = "Up"
+    elif not button_U_state and button_D_state and not button_L_state and not button_R_state and not button_C_state:
+        text = "Down"
+    elif not button_U_state and not button_D_state and button_L_state and not button_R_state and not button_C_state:
+        text = "Left"
+    elif not button_U_state and not button_D_state and not button_L_state and button_R_state and not button_C_state:
+        text = "Right"
+    elif not button_U_state and not button_D_state and not button_L_state and not button_R_state and button_C_state:
+        text = "Center Button Pressed"
     else:
-        text = "No buttons pressed"
+        text = "No Joystick Button Pressed"
 
     # Draw text on the image
     draw.text((10 + (joystick_x_value - min_x) // 100, 10 + (joystick_y_value - min_y) // 100), text, font=font, fill=1)
