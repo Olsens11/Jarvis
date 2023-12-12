42a4553c14aebd2a4e16cb02dfad57794cd42ada
Olsens11 <Olsens11@gmail.com>
2023-12-11 20:40:05 -0500

    Update all files and folders

--- bonnet.py
+++ bonnet.py
@@ -5,7 +5,7 @@ import adafruit_ssd1306
 
 # Set up the OLED Bonnet
 reset_pin = DigitalInOut(board.D4)
-i2c = board.I2C(board.SCL, board.SDA)
+i2c = board.I2C()
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin)
 
 # Create blank image for drawing.
