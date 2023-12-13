a3b92f29272bc7f71c905708a0bf427603af37d5
Olsens11 <Olsens11@gmail.com>
2023-12-13 01:54:16 -0500

    Update all files and folders

--- peeky1.py
+++ peeky1.py
@@ -1,127 +1,161 @@
 import os
-import time
 import board
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-from PIL import Image, ImageDraw, ImageFont
+import digitalio
 import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
 
-# Initialize I2C and OLED display
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+# Set up the OLED Bonnet
+reset_pin = digitalio.DigitalInOut(board.D4)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin)
 
-# Input pins:
-button_A = DigitalInOut(board.D5)
-button_A.direction = Direction.INPUT
-button_A.pull = Pull.UP
+# Create an image with a black background
+image = Image.new("1", (oled.width, oled.height), 0)
+draw = ImageDraw.Draw(image)
 
-button_B = DigitalInOut(board.D6)
-button_B.direction = Direction.INPUT
-button_B.pull = Pull.UP
+# Load a font
+font = ImageFont.load_default()
 
-button_L = DigitalInOut(board.D27)
-button_L.direction = Direction.INPUT
-button_L.pull = Pull.UP
+# Initialize buttons
+button_A = digitalio.DigitalInOut(board.D5)
+button_A.switch_to_input(pull=digitalio.Pull.UP)
 
-button_R = DigitalInOut(board.D23)
-button_R.direction = Direction.INPUT
-button_R.pull = Pull.UP
+button_B = digitalio.DigitalInOut(board.D6)
+button_B.switch_to_input(pull=digitalio.Pull.UP)
 
-button_U = DigitalInOut(board.D17)
-button_U.direction = Direction.INPUT
-button_U.pull = Pull.UP
+button_L = digitalio.DigitalInOut(board.D27)
+button_L.switch_to_input(pull=digitalio.Pull.UP)
 
-button_D = DigitalInOut(board.D22)
-button_D.direction = Direction.INPUT
-button_D.pull = Pull.UP
+button_R = digitalio.DigitalInOut(board.D23)
+button_R.switch_to_input(pull=digitalio.Pull.UP)
 
-button_C = DigitalInOut(board.D4)
-button_C.direction = Direction.INPUT
-button_C.pull = Pull.UP
+button_U = digitalio.DigitalInOut(board.D17)
+button_U.switch_to_input(pull=digitalio.Pull.UP)
 
-# Constants
-WHITE = 1
-BLACK = 0
-DISPLAY_WIDTH = 128
-DISPLAY_HEIGHT = 64
-FONT_SIZE = 10
-MAX_FILE_NAME_LENGTH = 16  # Adjust based on your preference
+button_D = digitalio.DigitalInOut(board.D22)
+button_D.switch_to_input(pull=digitalio.Pull.UP)
 
-# Load font
-font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
-font = ImageFont.truetype(font_path, FONT_SIZE)
+button_C = digitalio.DigitalInOut(board.D4)
+button_C.switch_to_input(pull=digitalio.Pull.UP)
 
-# Function to display text on OLED
-def display_text(text, color=WHITE):
-    oled.fill(0)
-    draw = ImageDraw.Draw(oled)
-    draw.text((0, 0), text, font=font, fill=color)
-    oled.show()
+# Rectangles configuration
+rect_width = 40
+rect_height = 12
+rect_margin_x = 1
+rect_margin_y = 1
 
-# Function to display menu options
-def display_menu(options, selected_index):
-    oled.fill(0)
-    draw = ImageDraw.Draw(oled)
-    for i, option in enumerate(options):
-        if i == selected_index:
-            draw.rectangle((0, i * (FONT_SIZE + 2), DISPLAY_WIDTH, i * (FONT_SIZE + 2) + FONT_SIZE + 2), outline=WHITE, fill=WHITE)
-            draw.text((2, i * (FONT_SIZE + 2) + 2), option, font=font, fill=BLACK)
-        else:
-            draw.text((2, i * (FONT_SIZE + 2) + 2), option, font=font, fill=WHITE)
-    oled.show()
+# Filename rectangles configuration
+filename_rect_width = 122
+filename_rect_height = 12
+filename_margin_y = 1
 
-# Function to handle file/folder selection
-def select_item(items, selected_index):
-    return items[selected_index]
-
-# Function to display and handle favorites screen
-def display_favorites(favorites, selected_index):
-    display_menu(favorites, selected_index)
-    # Handle user input and add to favorites logic here
-
-# Function to display and handle settings screen
-def display_settings(selected_index):
-    settings = ["Invert Black/White", "Rotate Screen", "Close Program"]
-    display_menu(settings, selected_index)
-    # Handle user input and settings logic here
-
-# Function to browse and display files in a directory
-def browse_directory(path):
-    files = os.listdir(path)
-    return [file[:MAX_FILE_NAME_LENGTH] for file in files]
-
-# Main program
-current_path = "/"
+# Initial selected rectangle index
 selected_index = 0
-favorites = []
 
+# Current directory path
+current_path = "/"
+
+# Words and their estimated widths
+words = ["Back", "Faves", "Setup"]
+word_widths = [24, 30, 30]
+
+def get_displayed_files(directory):
+    return os.listdir(directory)
+
+# Main loop
 while True:
-    current_items = browse_directory(current_path)
+    # Clear the image
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
-    # Display top menu
-    menu_options = ["Back", "Favorites", "Settings"]
-    display_menu(menu_options, selected_index)
+    # Read button states
+    button_A_state = not button_A.value
+    button_B_state = not button_B.value
+    button_L_state = not button_L.value
+    button_R_state = not button_R.value
+    button_U_state = not button_U.value
+    button_D_state = not button_D.value
+    button_C_state = not button_C.value
 
-    # Display file/folder names
-    display_menu(current_items, selected_index)
+    # Update selected index based on directional buttons
+    if button_U_state:
+        selected_index = (selected_index - 1) % (3 + len(os.listdir(current_path)))
+        while not button_U.value:  # Wait until button is released
+            pass
+    elif button_D_state:
+        selected_index = (selected_index + 1) % (3 + len(os.listdir(current_path)))
+        while not button_D.value:  # Wait until button is released
+            pass
+    elif button_L_state:
+        selected_index = (selected_index + 1) % (3 + len(os.listdir(current_path)))  # Adjusted for left
+        while not button_L.value:  # Wait until button is released
+            pass
+    elif button_R_state:
+        selected_index = (selected_index - 1) % (3 + len(os.listdir(current_path)))  # Adjusted for right
+        while not button_R.value:  # Wait until button is released
+            pass
 
-    # Handle user input
-    if not button_U.value:
-        selected_index = max(0, selected_index - 1)
-    elif not button_D.value:
-        selected_index = min(len(current_items) - 1, selected_index + 1)
-    elif not button_C.value:
-        if selected_index == 0:  # Back
+    # Handle center button press
+    if button_C_state:
+        if selected_index == 0:  # Back rectangle selected
             current_path = os.path.dirname(current_path)
-        elif selected_index == 1:  # Favorites
-            display_favorites(favorites, 0)
-        elif selected_index == 2:  # Settings
-            display_settings(0)
-    elif not button_A.value:
-        # Additional logic for button A
-        pass
-    elif not button_B.value:
-        # Additional logic for button B
-        pass
+            while not button_C.value:  # Wait until button is released
+                pass
+        elif selected_index > 2:  # Filename rectangle selected
+            selected_file = os.listdir(current_path)[len(os.listdir(current_path)) - selected_index + 2]
+            selected_path = os.path.join(current_path, selected_file)
+            if os.path.isdir(selected_path):
+                current_path = selected_path
+                while not button_C.value:  # Wait until button is released
+                    pass
 
-    time.sleep(0.1)  # Adjust sleep duration as needed
+    # Draw rectangles and text at the top
+    for i in range(3):
+        x = i * (rect_width + rect_margin_x)
+        y = rect_margin_y
+
+        # Check if the rectangle is selected
+        is_selected = i == selected_index
+
+        # Calculate the starting position to center the word within the rectangle
+        text_x = x + (rect_width - word_widths[i]) // 2
+        text_y = y + rect_margin_y - 1
+
+        # Draw the rectangle
+        draw.rectangle(
+            (x, y, x + rect_width, y + rect_height),
+            outline=1 if not is_selected else 0,
+            fill=1 if is_selected else 0,
+        )
+
+        # Draw the text
+        draw.text((text_x, text_y), words[i], font=font, fill=0 if is_selected else 1)
+
+    # Display file names in the current directory below the rectangles
+    displayed_files = get_displayed_files(current_path)
+    for i, file_name in enumerate(reversed(displayed_files)):
+        file_y = rect_margin_y + (i + 1) * (filename_rect_height + rect_margin_y)
+
+        # Check if the file rectangle is selected
+        is_file_selected = selected_index == 3 + len(displayed_files) - 1 - i
+
+        # Draw the file rectangle
+        draw.rectangle(
+            (0, file_y, filename_rect_width, file_y + filename_rect_height),
+            outline=1 if not is_file_selected else 0,
+            fill=1 if is_file_selected else 0,
+        )
+
+        # Draw the file text
+        draw.text(
+            (rect_margin_x, file_y + filename_margin_y - 1),
+            file_name,
+            font=font,
+            fill=0 if is_file_selected else 1,
+        )
+
+    # Rotate the image 180 degrees before displaying
+    rotated_image = image.rotate(180)
+
+    # Display the rotated image on the OLED
+    oled.image(rotated_image)
+    oled.show()
