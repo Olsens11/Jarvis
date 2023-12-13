c4ab0081b117268c725e13d878befe46c44bd4b6
Olsens11 <Olsens11@gmail.com>
2023-12-12 22:33:07 -0500

    Update all files and folders

--- grr.sh
+++ grr.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Step 1: Download the latest Adafruit CircuitPython bundle
-wget https://circuitpython.org/libraries/all_releases.atom -O all_releases.atom
-
-# Step 2: Install Adafruit Blinka
+# Step 1: Install Adafruit Blinka
 sudo pip3 install --upgrade adafruit-blinka
 
-# Step 3: Install the required libraries from the downloaded bundle
-sudo pip3 install $(cat all_releases.atom | grep 'link href' | grep -Eo "(https|http)://[a-zA-Z0-9./?=_-]*tar.gz" | sed 's/\.tar\.gz//g')
+# Step 2: Download and install the latest Adafruit CircuitPython bundle
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/adafruit/Adafruit_CircuitPython_Bundle/releases/latest | grep "tag_name" | cut -d'"' -f4)
+sudo pip3 install https://github.com/adafruit/Adafruit_CircuitPython_Bundle/archive/$LATEST_RELEASE.tar.gz
 
-# Step 4: Clean up the downloaded atom file
-rm all_releases.atom
+# Step 3: Reboot the Raspberry Pi
+sudo reboot
