c5d4e85bc3449a2eb9cb0c9896efcf839e08869c
Olsens11 <Olsens11@gmail.com>
2023-12-13 00:17:19 -0500

    Update all files and folders

--- peeky.py
+++ peeky.py
@@ -1,91 +1,87 @@
-import os
-import time
 import board
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-from PIL import Image, ImageDraw, ImageFont
+import digitalio
 import adafruit_ssd1306
-from font5x8 import font_data  # Import the font data
+from PIL import Image, ImageDraw, ImageFont
 
-# Constants
-WHITE = 1
-BLACK = 0
-DISPLAY_WIDTH = 128
-DISPLAY_HEIGHT = 64
-FONT_SIZE = 8  # Change this value as needed
-MAX_FILE_NAME_LENGTH = 16  # Adjust based on your preference
+# Set up the OLED Bonnet
+reset_pin = digitalio.DigitalInOut(board.D4)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, reset=reset_pin)
 
-# Initialize I2C and OLED display
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+# Create an image with a black background
+image = Image.new("1", (oled.width, oled.height), 0)
+draw = ImageDraw.Draw(image)
 
-# Load font
-font = ImageFont.load_default()  # Use the default font
-
-# Function to display text on OLED using ImageDraw
-def display_text(text, color=WHITE):
-    oled.fill(0)
-    draw = ImageDraw.Draw(oled.image)
-    draw.text((0, 0), text, font=font, fill=color)
-    oled.show()
-
-# Function to browse and display files in a directory
-def browse_directory(path):
-    try:
-        files = os.listdir(path)
-        return [file[:MAX_FILE_NAME_LENGTH] for file in files]
-    except Exception as e:
-        print(f"Exception during browse_directory: {e}")
-        return []
-
-# Main program
-current_path = "/"
-selected_index = 0
+# Load a font
+font = ImageFont.load_default()
 
 # Initialize buttons
-button_A = DigitalInOut(board.D5)
-button_A.direction = Direction.INPUT
-button_A.pull = Pull.UP
+button_U = digitalio.DigitalInOut(board.D17)
+button_U.switch_to_input(pull=digitalio.Pull.UP)
 
-button_B = DigitalInOut(board.D6)
-button_B.direction = Direction.INPUT
-button_B.pull = Pull.UP
+button_D = digitalio.DigitalInOut(board.D22)
+button_D.switch_to_input(pull=digitalio.Pull.UP)
 
+button_R = digitalio.DigitalInOut(board.D23)
+button_R.switch_to_input(pull=digitalio.Pull.UP)
+
+button_L = digitalio.DigitalInOut(board.D27)
+button_L.switch_to_input(pull=digitalio.Pull.UP)
+
+# Rectangles configuration
+rect_width = 40
+rect_height = 20
+rect_margin = 10
+
+# Initial selected rectangle index
+selected_index = 0
+
+# Main loop
 while True:
-    try:
-        current_items = browse_directory(current_path)
+    # Clear the image
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
-        # Read button states
-        button_A_state = not button_A.value
-        button_B_state = not button_B.value
+    # Read button states
+    button_U_state = not button_U.value
+    button_D_state = not button_D.value
+    button_R_state = not button_R.value
+    button_L_state = not button_L.value
 
-        # Determine text based on button states
-        if button_A_state and button_B_state:
-            text = "Both buttons pressed"
-        elif button_A_state:
-            text = "Button A pressed"
-        elif button_B_state:
-            text = "Button B pressed"
-        else:
-            # Display file/folder names
-            text = current_items[selected_index]
+    # Update selected index based on directional buttons
+    if button_U_state:
+        selected_index = (selected_index + 1) % 3
+        while not button_U.value:  # Wait until button is released
+            pass
+    elif button_D_state:
+        selected_index = (selected_index - 1) % 3
+        while not button_D.value:  # Wait until button is released
+            pass
 
-        # Display text on OLED
-        display_text(text, WHITE)
+    # Draw rectangles and text
+    for i in range(3):
+        x = i * (rect_width + rect_margin)
+        y = rect_margin
 
-        # Handle user input
-        if not button_A.value:
-            selected_index = max(0, selected_index - 1)
-            time.sleep(0.2)  # Debounce
-        elif not button_B.value:
-            selected_index = min(len(current_items) - 1, selected_index + 1)
-            time.sleep(0.2)  # Debounce
-        elif button_A.value and button_B.value:
-            if selected_index == 0:  # Back
-                current_path = os.path.dirname(current_path)
-            time.sleep(0.2)  # Debounce
+        # Check if the rectangle is selected
+        is_selected = i == selected_index
 
-    except Exception as e:
-        print(f"Main loop exception: {e}")
+        # Draw the rectangle
+        draw.rectangle(
+            (x, y, x + rect_width, y + rect_height),
+            outline=1 if not is_selected else 0,
+            fill=1 if is_selected else 0,
+        )
 
-    time.sleep(0.1)  # Adjust sleep duration as needed
+        # Draw the text
+        text = ["Back", "Favorites", "Settings"][i]
+        text_width, text_height = draw.textsize(text, font)
+        text_x = x + (rect_width - text_width) // 2
+        text_y = y + (rect_height - text_height) // 2
+        draw.text((text_x, text_y), text, font=font, fill=0 if is_selected else 1)
+
+    # Rotate the image 180 degrees before displaying
+    rotated_image = image.rotate(180)
+
+    # Display the rotated image on the OLED
+    oled.image(rotated_image)
+    oled.show()
