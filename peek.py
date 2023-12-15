ddffd975f44ad41f496fed359293953bff087e53
Olsens11 <Olsens11@gmail.com>
2023-12-14 19:49:00 -0500

    Update all files and folders

--- peek.py
+++ peek.py
@@ -1,3 +1,4 @@
+import os
 import board
 import digitalio
 import adafruit_ssd1306
@@ -16,30 +17,55 @@ draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 
 # Initialize buttons
+button_A = digitalio.DigitalInOut(board.D5)
+button_A.switch_to_input(pull=digitalio.Pull.UP)
+
+button_B = digitalio.DigitalInOut(board.D6)
+button_B.switch_to_input(pull=digitalio.Pull.UP)
+
+button_L = digitalio.DigitalInOut(board.D27)
+button_L.switch_to_input(pull=digitalio.Pull.UP)
+
+button_R = digitalio.DigitalInOut(board.D23)
+button_R.switch_to_input(pull=digitalio.Pull.UP)
+
 button_U = digitalio.DigitalInOut(board.D17)
 button_U.switch_to_input(pull=digitalio.Pull.UP)
 
 button_D = digitalio.DigitalInOut(board.D22)
 button_D.switch_to_input(pull=digitalio.Pull.UP)
 
-button_R = digitalio.DigitalInOut(board.D23)
-button_R.switch_to_input(pull=digitalio.Pull.UP)
+button_C = digitalio.DigitalInOut(board.D4)
+button_C.switch_to_input(pull=digitalio.Pull.UP)
 
-button_L = digitalio.DigitalInOut(board.D27)
-button_L.switch_to_input(pull=digitalio.Pull.UP)
-
-# Rectangles configuration
+# Rectangles and square configuration
 rect_width = 40
 rect_height = 12
 rect_margin_x = 1
 rect_margin_y = 1
 
+square_width = 12
+square_height = rect_height + 2  # Make the square the same height as the rectangles
+
+# Adjust the rectangle width and spacing
+rect_width -= square_width + rect_margin_x  # Reduce rectangle width by square width
+rect_margin_x = 1  # Maintain consistent spacing
+
 # Initial selected rectangle index
 selected_index = 0
+
+# Previous selected index
+prev_selected_index = 0
+
+# Current directory path
+current_path = "/"
 
 # Words and their estimated widths
 words = ["Back", "Faves", "Setup"]
 word_widths = [24, 30, 30]
+
+def get_displayed_files(directory):
+    return os.listdir(directory)
 
 # Main loop
 while True:
@@ -47,32 +73,81 @@ while True:
     draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
     # Read button states
+    button_A_state = not button_A.value
+    button_B_state = not button_B.value
+    button_L_state = not button_L.value
+    button_R_state = not button_R.value
     button_U_state = not button_U.value
     button_D_state = not button_D.value
-    button_R_state = not button_R.value
-    button_L_state = not button_L.value
+    button_C_state = not button_C.value
 
     # Update selected index based on directional buttons
     if button_U_state:
-        selected_index = (selected_index - 1) % 3
+        if selected_index == 2 or selected_index > 2:
+            selected_index = (selected_index - 1) % (3 + len(os.listdir(current_path)))
         while not button_U.value:  # Wait until button is released
             pass
     elif button_D_state:
-        selected_index = (selected_index + 1) % 3
+        if selected_index > 2:
+            selected_index = (selected_index + 1) % (3 + len(os.listdir(current_path)))
         while not button_D.value:  # Wait until button is released
             pass
     elif button_L_state:
-        selected_index = (selected_index + 1) % 3  # Adjusted for left
+        if selected_index == 0:  # Back rectangle selected
+            selected_index = 1  # Move to Faves
+        elif selected_index == 1:  # Faves rectangle selected
+            selected_index = 0  # Move to Back
+        elif selected_index == 2:  # Setup rectangle selected
+            selected_index = 1  # Move to Faves
         while not button_L.value:  # Wait until button is released
             pass
     elif button_R_state:
-        selected_index = (selected_index - 1) % 3  # Adjusted for right
+        if selected_index == 0:  # Back rectangle selected
+            selected_index = 2  # Move to Setup
+        elif selected_index == 1:  # Faves rectangle selected
+            selected_index = 2  # Move to Setup
+        elif selected_index == 2:  # Setup rectangle selected
+            selected_index = 0  # Move to Back
         while not button_R.value:  # Wait until button is released
             pass
 
-    # Draw rectangles and text
+    # Handle center button press
+    if button_C_state:
+        if selected_index == 0 or selected_index == 1:  # Back or Faves rectangle selected
+            current_path = os.path.dirname(current_path)
+            prev_selected_index = selected_index
+            selected_index = 3  # Select the uppermost filename
+            while not button_C.value:  # Wait until button is released
+                pass
+        elif selected_index > 2:  # Filename rectangle selected
+            selected_file = os.listdir(current_path)[len(os.listdir(current_path)) - selected_index + 2]
+            selected_path = os.path.join(current_path, selected_file)
+            if os.path.isdir(selected_path):
+                current_path = selected_path
+                prev_selected_index = selected_index
+                selected_index = 3  # Select the uppermost filename
+                while not button_C.value:  # Wait until button is released
+                    pass
+
+    # Handle B button press (acts as Back button)
+    if button_B_state:
+        current_path = os.path.dirname(current_path)
+        selected_index = prev_selected_index
+        while not button_B.value:  # Wait until button is released
+            pass
+
+    # Draw the square to the left of the rectangles
+    square_x = 0
+    square_y = rect_margin_y
+    draw.rectangle(
+        (square_x, square_y, square_x + square_width, square_y + square_height),
+        outline=1 if selected_index == 0 else 0,
+        fill=1 if selected_index == 0 else 0,
+    )
+
+    # Draw rectangles and text at the top
     for i in range(3):
-        x = i * (rect_width + rect_margin_x)
+        x = square_width + i * (rect_width + rect_margin_x)
         y = rect_margin_y
 
         # Check if the rectangle is selected
@@ -91,6 +166,29 @@ while True:
 
         # Draw the text
         draw.text((text_x, text_y), words[i], font=font, fill=0 if is_selected else 1)
+
+    # Display file names in the current directory below the rectangles
+    displayed_files = get_displayed_files(current_path)
+    for i, file_name in enumerate(reversed(displayed_files)):
+        file_y = rect_margin_y + (i + 1) * (filename_rect_height + rect_margin_y)
+
+        # Check if the filename rectangle is selected
+        is_selected = i + 3 == selected_index
+
+        # Draw the filename rectangle
+        draw.rectangle(
+            (square_width, file_y, square_width + filename_rect_width, file_y + filename_rect_height),
+            outline=1 if not is_selected else 0,
+            fill=1 if is_selected else 0,
+        )
+
+        # Draw the filename text
+        draw.text(
+            (square_width + 1, file_y + 1),
+            file_name if len(file_name) <= max_filename_length else file_name[:max_filename_length - 3] + "...",
+            font=font,
+            fill=0 if is_selected else 1,
+        )
 
     # Rotate the image 180 degrees before displaying
     rotated_image = image.rotate(180)
