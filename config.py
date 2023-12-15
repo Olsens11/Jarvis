e09aa29c36dd21e5ff84d05221f14b90a1ec111f
Olsens11 <Olsens11@gmail.com>
2023-12-15 00:18:59 -0500

    Update all files and folders

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ rect_width = 38  # Width of the top rectangles
 rect_height = 12  # Height of the top rectangles
 rect_margin_x = 1  # Horizontal margin between top rectangles
 rect_margin_y = 1  # Vertical margin between top rectangles
+rect_outline_color_selected = 0
+rect_outline_color_unselected = 1
+rect_fill_color_selected = 0
+rect_fill_color_unselected = 1
 
 # Dimensions for filename rectangles
 filename_rect_width = 124  # Width of the filename rectangles
@@ -32,3 +36,25 @@ text_alignment = "left"  # Text alignment ("left", "center", "right")
 
 # Square line parameters
 square_line_color = 1  # Color for lines in the square (1 for white, 0 for black)
+square_line_color_selected = 0
+square_line_color_unselected = 1
+square_line_spacing
+
+square_outline_color_selected = 0
+square_outline_color_unselected = 1
+square_fill_color_selected = 0
+square_fill_color_unselected = 1
+square_outline_color = 1
+
+vert_rect_x
+vert_rect_y
+vert_rect_width = 12
+vert_rect_height = 54
+vert_rect_outline_color_selected = 0
+vert_rect_outline_color_unselected = 1
+vert_rect_fill_color_selected = 0
+vert_rect_fill_color_unselected = 1
+
+
+####################################################################################################
+####################################################################################################
