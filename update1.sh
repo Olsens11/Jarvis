0284d98b55195ed51eed7bf23bb16e62630cd0c5
Olsens11 <Olsens11@gmail.com>
2023-12-14 20:31:45 -0500

    Update all files and folders

--- update1.sh
+++ update1.sh
@@ -2,26 +2,9 @@
 
 # Commit changes
 git add --all
-git commit -m "Automated commit before pull"
-
-# Check if the commit was successful
-if [ $? -eq 0 ]; then
-    echo "Commit successful."
-else
-    echo "Error: Commit failed."
-    exit 1
-fi
+git commit -m "Automated commit before pull" || { echo "Error: Commit failed."; exit 1; }
 
 # Pull changes from the "main" branch
-git pull origin main
+git pull origin main || { echo "Error: Pull failed."; exit 1; }
 
-# Check if the pull was successful
-if [ $? -eq 0 ]; then
-    echo "Pull successful."
-else
-    echo "Error: Pull failed."
-    exit 1
-fi
-
-# Pause to see the output (remove this line if you don't want to pause)
-read -p "Press Enter to continue..."
+echo "Script completed successfully."
